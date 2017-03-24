16f47e1eeeb8433097a9c9ead0c5de1c218f4f73
ALKAN ULUTAŞ <ulutasalkan@hotmail.com>
2017-03-25 00:10:22 +0300

    RunCode

---
+++ App.java
@@ -0,0 +1,12 @@
+package Homework_Java_020;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		BigInt num=new BigInt("99999999999999999999999999999999999999999999999999");
+		BigInt num1=new BigInt(988888888);
+		num1.display();
+	}
+
+}
